23270e5ecad73d8561fe9da7ad0a80cae3ec9665
Danail Danailov <131172818+danailov1@users.noreply.github.com>
2024-10-20 09:38:25 +0300

    Add files via upload

---
+++ piramidgenerator.js
@@ -0,0 +1,14 @@
+const character = "!";
+const count = 10;
+const rows = [];
+let inverted = false;
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
